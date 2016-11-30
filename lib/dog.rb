96c0f0ee2c6caee7db20cee07af035458628128e
github username <github email address>
2016-11-30 07:40:14 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,7 @@
+class Dog
+  #definition
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+class Person
+  #definition
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
